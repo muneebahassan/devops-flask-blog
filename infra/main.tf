d09aa990e198aada8d17f6090bf573dd4429435b
muneebahassan <theailaboratories@gmail.com>
2025-04-20 00:12:24 +0500

    Fix: Remove duplicate output definition

--- infra/main.tf
+++ infra/main.tf
@@ -58,8 +58,3 @@ resource "aws_instance" "devops_instance" {
     ]
   }
 }
-
-output "instance_public_ip" {
-  description = "The public IP of the EC2 instance"
-  value       = aws_instance.devops_instance.public_ip
-}
